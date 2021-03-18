a581a62647df5126a067aeadd09a65b90109e6b3
Melissa DElia <d0echk@aol.com>
2021-03-18 10:47:01 -0700

    initial commit

---
+++ designs.js
@@ -0,0 +1,52 @@
+// Select color input and assign to a variable
+const color = document.querySelector('#colorPicker');
+const table = document.querySelector('#pixelCanvas');
+// Select size input
+const formSize = document.querySelector('#sizePicker')
+//I want to see if this shows up in my git
+
+
+    
+    
+
+
+
+formSize.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const height = document.querySelector('#inputHeight').value;
+    const width = document.querySelector('#inputWidth').value;
+
+    table.innerHTML = null;    
+    makeGrid(height, width);
+});    
+
+
+
+
+
+
+// When size is submitted by the user, call makeGrid()
+// Your code goes here!
+
+function makeGrid(height, width) {
+    
+for (let i = 0; i < height; i++) {
+    let row = table.insertRow(i);
+    
+    for (let x = 0; x < width; x++) {
+        let cell = row.insertCell(x);
+         
+        cell.addEventListener('click', function(evt){ 
+            evt.preventDefault();
+            cell.style.backgroundColor = color.value;
+            cell.style.border = 0;
+            row.style.border = 0;
+        });
+       
+    }
+}      
+}
+
+
+
